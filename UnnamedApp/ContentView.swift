cf585ab833688f9ade9861fbedfc3626920d83c2
Akylbek <akke.utekeshev@gmail.com>
2021-03-31 16:45:27 +0600

    Initial Commit

---
+++ UnnamedApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnnamedApp
+//
+//  Created by Akylbek Utekeshev on 3/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnnamedApp/UnnamedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnnamedAppApp.swift
+//  UnnamedApp
+//
+//  Created by Akylbek Utekeshev on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct UnnamedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
